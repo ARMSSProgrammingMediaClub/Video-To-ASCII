41d4f4026449e48ce3a1e36cdd2c692e0e90f811
hjyynck <146595235+hjyynck@users.noreply.github.com>
2024-07-04 16:59:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import numpy as np
 import cv2 as cv
 import os
 
-width = 120 # 120 Default # 150 Optimal
-height = 32 # 32 Default # 40 Optimal
+width = 120 # 120 Default
+height = 32 # 32 Default
 cap = cv.VideoCapture("C:\\Path\\To\\Video\\Goes\\Here")
 gradient = '- : _ , ^ = ; > < + ! r c * / z ? s L T v ) J 7 ( | F i f I 3 1 t l u [ n e o Z 5 Y x j y a ] 2 E S w q k P 6 h 9 d 4 V p O G b U A K X H m 8 R D # $ B g 0 M N W Q % & @'.split(' ')
 cmd = (f'mode {width},{height}')
